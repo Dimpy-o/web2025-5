d59c324012ae689f39c4a5d3927d95f6abc12d0a
Kapustin Denys web2025-5 <deniskapustin4@gmail.com>
2025-05-28 20:46:19 +0300

    Add requests to http.cat

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { program } = require('commander');
 const fs = require('fs').promises;
 const http = require('http');
 const path = require('path');
+const superagent = require('superagent');
 
 program
   .requiredOption('-h, --host <host>', 'адреса сервера')
@@ -16,46 +17,55 @@ const port = options.port
 const cache = options.cache
 
 const server = http.createServer(async (req, res) => {
-  if (!['GET', 'PUT', 'DELETE'].includes(req.method)) {
-    res.writeHead(405, { 'Allow': 'GET, PUT, DELETE' });
-    return res.end();
-  }
-
-  const code = req.url.slice(1);
-
-  const filePath = path.join(cache, `${code}.jpg`);
-
-  switch (req.method) {
-    case 'GET':
-      try {
-        const image = await fs.readFile(filePath);
-        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-        return res.end(image);
-      } catch {
-        res.writeHead(404);
+    if (!['GET', 'PUT', 'DELETE'].includes(req.method)) {
+        res.writeHead(405, { 'Allow': 'GET, PUT, DELETE' });
         return res.end();
-      }
+    }
 
-    case 'PUT':
-      const chunks = [];
-      for await (const chunk of req) {
-        chunks.push(chunk);
-      }
-      await fs.writeFile(filePath, Buffer.concat(chunks));
-      res.writeHead(201);
-      return res.end();
+    const code = req.url.slice(1);
 
-    case 'DELETE':
-      try {
-        await fs.unlink(filePath);
-        res.writeHead(200);
-        return res.end();
-      } catch {
-        res.writeHead(404);
-        return res.end();
-      }
-  }
-});
+    const filePath = path.join(cache, `${code}.jpg`);
+
+    switch (req.method) {
+        case 'GET':
+            try {
+                const image = await fs.readFile(filePath);
+                res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+                return res.end(image);
+            } catch {
+                try {
+                    const response = await superagent.get(`https://http.cat/${code}`)
+                        .responseType('blob');
+
+                    await fs.writeFile(filePath, response.body);
+                    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+                    return res.end(response.body);
+                } catch {
+                    res.writeHead(404);
+                    return res.end();
+                }
+            }
+
+        case 'PUT':
+            const chunks = [];
+            for await (const chunk of req) {
+            chunks.push(chunk);
+            }
+            await fs.writeFile(filePath, Buffer.concat(chunks));
+            res.writeHead(201);
+            return res.end();
+
+        case 'DELETE':
+            try {
+                await fs.unlink(filePath);
+                res.writeHead(200);
+                return res.end();
+                } catch {
+                    res.writeHead(404);
+                    return res.end();
+                }
+            }
+        });
 
 server.listen(port, host, () => {
   console.log(`Server is running on http://${options.host}:${options.port}`);
